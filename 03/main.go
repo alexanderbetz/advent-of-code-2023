a59631b741efe195a62d603deafd1b1e1d6c5a21
Alexander Betz <alexander.betz@deckweiss.at>
2023-12-03 20:27:59 +0100

    feat: day 3 challenge 2

--- 03/main.go
+++ 03/main.go
@@ -8,6 +8,13 @@ import (
 	"strconv"
 )
 
+type Number struct {
+	value  int
+	xStart int
+	xEnd   int
+	y      int
+}
+
 func main() {
 	file, err := os.Open("input.txt")
 	if err != nil {
@@ -21,63 +28,101 @@ func main() {
 	}
 
 	var sumPartNumbers int = 0
+	var sumGearRatios int = 0
 
 	for lineIndex, line := range lines {
-		// minesweeper stuff
-		var numberStartIndex int = -1
-		var numberEndIndex int = -1
 		for charIndex := 0; charIndex < len(line); charIndex++ {
 			char := line[charIndex]
+
 			if isDigit(char) {
-				// set bounds for symbol lookup
-				numberStartIndex = charIndex
-				numberEndIndex = charIndex
+				number := getNumber(charIndex, line)
+				number.y = lineIndex
+
+				if isPartNumber(number, lines) {
+					sumPartNumbers += number.value
+					charIndex = number.xEnd
+				}
+			} else if char == '*' {
 				xStart := Max(0, charIndex-1)
 				xEnd := Min(len(line)-1, charIndex+1)
 				yStart := Max(0, lineIndex-1)
 				yEnd := Min(len(lines)-1, lineIndex+1)
 
-				isPartNumber := false
+				var numbers []Number
 
-				for x := xStart; x <= xEnd; x++ {
-					for y := yStart; y <= yEnd; y++ {
-						if !isDigit(lines[y][x]) && lines[y][x] != '.' {
-							isPartNumber = true
+				for y := yStart; y <= yEnd; y++ {
+					for x := xStart; x <= xEnd; x++ {
+						if isDigit(lines[y][x]) {
+							number := getNumber(x, lines[y])
+							number.y = y
+							numbers = append(numbers, number)
+							x = number.xEnd
 						}
 					}
 				}
 
-				if isPartNumber {
-					var startIndexFound bool = false
-					var endIndexFound bool = false
-					// look behind
-					for !startIndexFound {
-						if numberStartIndex-1 >= 0 && isDigit(line[numberStartIndex-1]) {
-							numberStartIndex--
-						} else {
-							startIndexFound = true
-						}
+				if len(numbers) == 2 {
+					n1 := numbers[0]
+					n2 := numbers[1]
+					if isPartNumber(n1, lines) && isPartNumber(n2, lines) {
+						sumGearRatios += n1.value * n2.value
 					}
-
-					// look ahead
-					for !endIndexFound {
-						if numberEndIndex+1 < len(line) && isDigit(line[numberEndIndex+1]) {
-							numberEndIndex++
-						} else {
-							endIndexFound = true
-						}
-					}
-
-					number, _ := strconv.Atoi(line[numberStartIndex : numberEndIndex+1])
-					sumPartNumbers += number
-
-					charIndex = numberEndIndex + 1
 				}
 			}
 		}
 	}
 
 	fmt.Printf("# valid part numbers: %d\n", sumPartNumbers)
+	fmt.Printf("sum of gear ratios: %d\n", sumGearRatios)
+}
+
+func isPartNumber(number Number, lines []string) bool {
+	isPartNumber := false
+
+	for x := number.xStart; x <= number.xEnd; x++ {
+		xStart := Max(0, x-1)
+		xEnd := Min(len(lines[number.y])-1, x+1)
+		yStart := Max(0, number.y-1)
+		yEnd := Min(len(lines)-1, number.y+1)
+
+		// minesweeper stuff
+		for x := xStart; x <= xEnd; x++ {
+			for y := yStart; y <= yEnd; y++ {
+				if !isDigit(lines[y][x]) && lines[y][x] != '.' {
+					isPartNumber = true
+				}
+			}
+		}
+	}
+	return isPartNumber
+}
+
+func getNumber(randomIndexWithinNumber int, line string) Number {
+	startIndex := randomIndexWithinNumber
+	endIndex := randomIndexWithinNumber
+	var startIndexFound bool = false
+	var endIndexFound bool = false
+
+	// look behind
+	for !startIndexFound {
+		if startIndex-1 >= 0 && isDigit(line[startIndex-1]) {
+			startIndex--
+		} else {
+			startIndexFound = true
+		}
+	}
+
+	// look ahead
+	for !endIndexFound {
+		if endIndex+1 < len(line) && isDigit(line[endIndex+1]) {
+			endIndex++
+		} else {
+			endIndexFound = true
+		}
+	}
+
+	number, _ := strconv.Atoi(line[startIndex : endIndex+1])
+	return Number{value: number, xStart: startIndex, xEnd: endIndex}
 }
 
 func isDigit(char byte) bool {
