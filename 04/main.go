41d525d69ea9fdd1a0fbc0db35ec1cf6667aabd8
Alexander Betz <alexander.betz@deckweiss.at>
2023-12-04 07:47:48 +0100

    feat: day 4 challenge 2

--- 04/main.go
+++ 04/main.go
@@ -19,6 +19,8 @@ func main() {
 	scanner := bufio.NewScanner(file)
 
 	var sumOfWinningMatches int = 0
+	var gameWinsCopies []int
+	var gameIndex int = 0
 	for scanner.Scan() {
 		line := scanner.Text()
 		numbers := strings.Split(strings.Split(line, ": ")[1], " | ")
@@ -31,6 +33,7 @@ func main() {
 		}
 
 		var score int = 0
+		gameWinsCopies = append(gameWinsCopies, 0)
 		for _, wn := range winningNumbers {
 			if myNumbers[wn] {
 				if score == 0 {
@@ -38,17 +41,39 @@ func main() {
 				} else {
 					score = score * 2
 				}
+				gameWinsCopies[gameIndex]++
 			}
 		}
 
 		sumOfWinningMatches += score
+		gameIndex++
 	}
 
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Sum of winning matches: %d\n", sumOfWinningMatches)
+	fmt.Printf("(Challenge 1) Sum of winning matches: %d\n", sumOfWinningMatches)
+	fmt.Printf("(Challenge 2) Sum of total scratch cards: %d\n", countTotalScratchCards(gameWinsCopies))
+}
+
+func countTotalScratchCards(scratchCardWinsCopies []int) int {
+	var totalScratchCards int = 0
+	var scratchCardExistsXTimes []int = make([]int, len(scratchCardWinsCopies))
+
+	for scratchCardIndex, scratchCardCopies := range scratchCardWinsCopies {
+		scratchCardExistsXTimes[scratchCardIndex]++
+
+		for i := 0; i < scratchCardCopies; i++ {
+			scratchCardExistsXTimes[scratchCardIndex+i+1] += scratchCardExistsXTimes[scratchCardIndex]
+		}
+	}
+
+	for _, existCount := range scratchCardExistsXTimes {
+		totalScratchCards += existCount
+	}
+
+	return totalScratchCards
 }
 
 func getNumbersFromString(line string) (numbers []int) {
